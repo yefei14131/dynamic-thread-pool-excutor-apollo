11d2959f776c1eef50b07842e75fefdb9dc71ce5
yefei <yefei@hualala.com>
2020-04-13 11:00:03 +0800

    demo bean config

--- src/main/java/com/hualala/qa/thread/pool/excutor/demo/DemoThreadPoolConfig.java
+++ src/main/java/com/hualala/qa/thread/pool/excutor/demo/DemoThreadPoolConfig.java
@@ -20,21 +20,23 @@ import java.util.concurrent.TimeUnit;
 /**
  * @author yefei
  * @date: 2020/4/13
+ *
  */
 //@Configuration  // 实际使用时放开此行注释
-//@EnableApolloConfig("qa.thread")  // 实际使用时放开此行注释
+//@EnableApolloConfig("qa.thread")  // 实际使用时放开此行注释。必须使用"qa.thread"这个namespace，因为 changeListener监听的是这个namespace
 public class DemoThreadPoolConfig {
 
-//    @Value("${demoExecutorBeanName.threadPool.corePoolSize}")  // 实际使用时放开此行注释
+
+//    @Value("${demoExecutorBeanName.threadPool.corePoolSize}")  // 实际使用时放开此行注释。demoExecutorBeanName为线程池beanName，必须一一对应。后缀".threadPool.corePoolSize"必须固定。
     private int corePoolSize;
 
-//    @Value("${demoExecutorBeanName.threadPool.maxPoolSize}")  // 实际使用时放开此行注释
+//    @Value("${demoExecutorBeanName.threadPool.maxPoolSize}")  // 实际使用时放开此行注释。demoExecutorBeanName为线程池beanName，必须一一对应。后缀".threadPool.maxPoolSize"必须固定。
     private int maxPoolSize;
 
-//    @Value("${demoExecutorBeanName.threadPool.maxQueueSize}")  // 实际使用时放开此行注释
+//    @Value("${demoExecutorBeanName.threadPool.maxQueueSize}")  // 实际使用时放开此行注释。demoExecutorBeanName为线程池beanName，必须一一对应。后缀".threadPool.maxQueueSize"必须固定。
     private int maxQueueSize;
 
-//    @Bean(name = "demoExecutorBeanName")  // 实际使用时放开此行注释
+//    @Bean(name = "demoExecutorBeanName")  // 实际使用时放开此行注释。
     public ThreadPoolExecutor threadPoolExecutor3(){
         return new ThreadPoolExecutor(corePoolSize, maxPoolSize, 60, TimeUnit.SECONDS, new ResizeableCapacityLinkedBlockIngQueue<>(maxQueueSize));
     }
